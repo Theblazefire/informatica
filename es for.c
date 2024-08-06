70953ed68e3dab360758d29cd1c7ea8f20a75900
DrCh0pper <marco.roncacci@gmail.com>
2024-08-06 14:48:26 +0200

    Update es for.c

--- es for.c
+++ es for.c
@@ -1,8 +1,8 @@
 //stampare da 0 a 99
 /*
-0 1 2 3
-4 5 6 7
-1=ix10^1+j*10^0
+quando i=0
+vine svolto il for j dove una volta che è stato completato 
+la i si incrementa ripetendo il ciclo
 
 */
 #include <stdio.h>
@@ -10,7 +10,7 @@ int main()
 {
 	for (int i = 0; i <=9; ++i)//decine
 	{
-		for (int j = 0; j <=9; ++j)
+		for (int j = 0; j <=9; ++j)//unità 
 		{
 			printf("%d\n", i*10+j);
 		}
