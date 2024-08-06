630daf45fc240625de99aa029f1052db73df81d7
DrCh0pper <marco.roncacci@gmail.com>
2024-08-06 17:58:53 +0200

    primo esercizio 2 esercitazione

---
+++ Ripasso/esercitazione2_es1.c
@@ -0,0 +1,56 @@
+/*
+============================================================== 
+ESERCIZIO 1: ISTRUZIONI CONDIZIONALI
+==============================================================
+
+Scrivi un programma che 
+chiede all'utente di 
+inserire tre numeri interi, 
+li legge e stampa un messaggio 
+che indica all'utente il valore 
+massimo ed il valore minimo fra quelli letti.
+
+specifica
+input (x1..x3)
+pre-condizione (x1..x3) app Z
+output n1 n2 app Z
+post-condizione n1=min(x1,x2,x3),n2=Max(x1,x2,x3)
+
+*/
+
+#include <stdio.h>
+int main()
+{
+	//input
+	int n=3,scatola[n];
+	printf("inserisci 3 numeri verifichero' quale sia il massimo e il minimo\n");
+	for (int i = 0; i < n; ++i)
+	{
+	  	printf("inserisci il %d della sequenza\n", i);
+	  	scanf_s("%d",&scatola[i]);
+	}
+	
+	int max=scatola[0];//dichiarazione del max
+    int min=scatola[0];//dichiarazione del min
+    //trova il massimo e il minimo
+	i=1;
+	while(i<=n)
+	{
+		if (scatola[i]>max)
+		{
+			max=scatola[i];
+		}
+		else
+		{
+			if (scatola[i]<min)
+			{
+				min=scatola[i];
+			}
+		}
+		i++;
+	}//fine while
+
+	printf("il max = %d , min=%d\n",max,min );
+
+	return 0;
+}
