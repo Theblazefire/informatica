a5b52d5ceebedae937d257668a643934ccf9bfbf
DrCh0pper <marco.roncacci@gmail.com>
2024-08-06 14:31:02 +0200

    Create es for.c

---
+++ es for.c
@@ -0,0 +1,19 @@
+//stampare da 0 a 99
+/*
+0 1 2 3
+4 5 6 7
+1=ix10^1+j*10^0
+
+*/
+#include <stdio.h>
+int main()
+{
+	for (int i = 0; i <= 9; ++i)
+	{
+		for (int j = 1; j < 9; ++j)
+		{
+			printf("%d\n",i*10^1+j*10^0 );
+		}
+	}
+	return 0;
+}
