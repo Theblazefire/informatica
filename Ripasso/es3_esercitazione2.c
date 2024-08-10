2cf6e5113ccc7b8d0b031db6415168b6bae41783
Theblazefire <100469738+Theblazefire@users.noreply.github.com>
2024-08-10 20:04:37 +0200

    es3 esercitazione 3 con diagramma

---
+++ Ripasso/es3_esercitazione2.c
@@ -0,0 +1,27 @@
+/*
+============================================================== 
+ESERCIZIO 3: ISTRUZIONI RIPETITIVE
+==============================================================
+
+Scrivi un programma che
+chiede all'utente di 
+inserire due interi x 
+ed y e stampa tutti gli 
+interi positivi che 
+sono compresi fra 
+x ed y (estremi inclusi) 
+e che non sono divisibili 
+per 3.
+
+specifica
+dati in entrata x e y
+pre-condizione x,y sono numeri interi
+dati in usicta stampa di (x...y)
+post-condizione (x...y) è composto da numeri interi non divisibili per 3
+*/
+#include <stdio.h>
+int main()
+{
+	
+	return 0;
+}
