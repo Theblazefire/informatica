d0eab59027c4e339cb2afc83aac01e111c9a8c23
DrCh0pper <marco.roncacci@gmail.com>
2024-08-24 20:32:08 +0200

    eso2017 fila A,B,C

--- esercitazioni/es1_eso_2017_B.c
+++ esercitazioni/es1_eso_2017_B.c
@@ -25,10 +25,54 @@ uno dei due interi della
 coppia divide l’altro se è falso
 non è vero 
 Problema di esistenza universare 
+(scatola[i]%(scatola[i+1]==0)||
+[-2, 6, 3, 9].. ..
+3 6 2 4
 */
 #include<stdio.h>
+int adiacentiDivisibili(int scatola[],int n)
+{
+	int q=1;//variabile booleana
+	int i=0;//variabile contatore
+	while(i<n-2 && q)
+	{
+		/*if ((scatola[i]%scatola[i+1]!=0)||(scatola[i+1]%scatola[i]!=0))
+		{
+			q=0;
+		}*/
+		if ((scatola[i]%scatola[i+1]!=0)||(scatola[i+1]%scatola[i]!=0))
+		{
+			q=0;
+		}
+		i++;
+	}
+	return q;//restituisci la variabile booleana
+
+}
+
 //funzione principale
 int main()
 {
+	/*input*/
+	printf("verifichero la proprieta'\nData una sequenza di interi, verificare se ciascuna coppia\n di interi adiacenti nella sequenza\n è tale che uno dei due interi\n della coppia divide l’altro\n");
+	int n;
+	printf("inserisci la lunghezza della sequenza\n");
+	scanf_s("%d",&n);
+	int scatola[n];//dichiara arrey monodimensionale
+	/*prendi elementi della sequenza*/
+	for (int i = 0; i < n; ++i)
+	{
+		printf("inserisci elemento %d della sequenza\n",i+1 );
+		scanf_s("%d",&scatola[i]);
+	}
+	int q=adiacentiDivisibili(scatola,n);
+	if (q)
+	{
+		printf("sodisfa la proprieta' nell'insieme \n");
+	}
+	else
+	{
+		printf("non sodisfa la proprieta' nell'insieme\n");
+	}
 
-}
+}//end main
---
+++ esercitazioni/es3_eso_2017_C.c
@@ -0,0 +1,72 @@
+/*
+Data una sequenza di interi, 
+verificare se tutte le terne 
+di elementi adiacenti nella 
+sequenza sono tali che uno 
+dei tre elementi è 
+pari alla somma degli altri due
+Specifica
+input (x1...xn)
+pre-condizione (x1...xn) app Z
+output q variabile booleana
+post-condizione q variabile booleana è
+vera se le terne 
+di elementi adiacenti nella 
+sequenza sono tali che uno 
+dei tre elementi è 
+pari alla somma degli altri due. se è falso
+non abbiamo somme degli elementi della terna 
+uguali hai elementi nella terna.
+
+problema di tipo universale  
+*/
+#include <stdio.h>
+int sommaTriple(int scat[],int n)
+{
+	int q=1;
+	int i=0;
+	while(i<n-2&&q)
+	{
+		if (// 1 0 5 6 8 5
+			!( 
+			  (scat[i]+scat[i+1]==scat[i+2]) || 
+			  (scat[i+1]+scat[i+2]==scat[i]) ||
+			  (scat[i]+scat[i+2]==scat[i+1])
+			 )
+		   )
+		{
+			q=0;
+		}
+		i++;
+		return q;
+	}
+
+}
+//funz principale
+int main()
+{
+	/*input*/
+	printf("sommo gli elementi di una tripla per trovare nella pripla un elemento che sia il risultato di una somma\n");
+	printf("lunghezza sequenza\n");
+	int n;
+	scanf("%d",&n);
+	int scatola[n];//arrey monodimensionale
+	/*prendi gli elementi della sequenza*/
+	for (int i = 0; i < n; ++i)
+	{
+		printf("elemento %d della sequenza\n",i+1 );
+		scanf("%d",&scatola[i]);
+	}
+	/*verifica la prop*/
+	int q=sommaTriple(scatola,n);
+	/*stampa*/
+	if (q)
+	{
+		printf("tutta la sequenza sodisfa la prop\n");
+	}
+	else
+	{
+		printf("non in tutta la sequenza sodisfa la prop\n");
+	}
+
+}
