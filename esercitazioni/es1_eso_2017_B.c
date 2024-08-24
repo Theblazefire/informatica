ee9bcd4719a666842b852107c329c3ba69db16f5
Theblazefire <100469738+Theblazefire@users.noreply.github.com>
2024-08-24 17:19:17 +0200

    es2_eso_2017_B

---
+++ esercitazioni/es1_eso_2017_B.c
@@ -0,0 +1,34 @@
+/*
+Si vuole progettare un algoritmo 
+per risolvere il seguente problema. 
+
+Data una sequenza di interi, 
+verificare se ciascuna coppia 
+di interi adiacenti nella sequenza 
+è tale che uno dei due interi 
+della coppia divide l’altro 
+
+(ovvero verificare se, per ogni 
+due elementi adiacenti nella 
+sequenza, il resto della divisione 
+del primo per il secondo è zero 
+o il resto della divisione del 
+secondo per il primo è zero)
+
+Specifica:
+input (x1...xn)
+pre-condizione (x1...xn) app Z
+
+output q variabile booleana
+post-condiozini q vale vero se esiste una coppia
+uno dei due interi della 
+coppia divide l’altro se è falso
+non è vero 
+Problema di esistenza universare 
+*/
+#include<stdio.h>
+//funzione principale
+int main()
+{
+
+}
