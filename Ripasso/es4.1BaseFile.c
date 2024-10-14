bf0b170d3646ce44b04f02fdc26ec90313a0071b
DrCh0pper <marco.roncacci@gmail.com>
2024-10-14 23:26:51 +0200

    .

--- Ripasso/es4.1BaseFile.c
+++ Ripasso/es4.1BaseFile.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+void copia(FILE *Ifile,FILE *Ofile)
+{
+	char c;
+	while(fscanf(Ifile,"%c",&c)>0)
+	{
+		fprintf(Ofile,"%c", c);
+	}
+}
 int main()
 {
 	FILE *Ifile,*Ofile;
+	char nume[50];
+	char numecopia[50];
+	printf("posso copiare un file di testo\n");
+	printf("inserisci il nume del file\n");
+	fgets(nume,50,stdin);
+	//elimina \n
+	nume[strlen(nume)-1]='\0';
+	printf("inserisci il nume del file copiato\n");
+	fgets(numecopia,50,stdin);
+	//elimina \n
+	numecopia[strlen(numecopia)-1]='\0';
+	/*copia file*/
+	Ifile=fopen(nume,"r");
+	Ofile=fopen(numecopia,"w");
+	copia(Ifile,Ofile);
+fclose(Ifile);
+fclose(Ofile);
 	return 0;
 }
