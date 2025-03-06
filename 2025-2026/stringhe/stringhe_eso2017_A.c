050cf1bcd58a32892502e5f67441f9cd6f0f56d3
TheBlazeFire <blezefire03@gmail.com>
2025-03-06 16:23:29 +0000

    corretto

--- 2025-2026/stringhe/stringhe_eso2017_A.c
+++ 2025-2026/stringhe/stringhe_eso2017_A.c
@@ -10,8 +10,8 @@ int main()
 char stringa[50];
 /*input*/
 printf("inserisci una stringa");
-fget(stringa,50,stdio);//prendi stringa
+fgets(stringa,50,stdin);//prendi stringa
 stringa[strlen(stringa)-1]='\0';
-printf('%s',stringa);
+printf("%s",stringa);
 
 }//chiusura main
