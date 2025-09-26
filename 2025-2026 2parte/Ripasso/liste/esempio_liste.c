c1ff9627aa0e647de201df92b2baf7482447705e
Theblazefire <100469738+Theblazefire@users.noreply.github.com>
2025-09-26 14:15:33 +0200

    inizio liste

---
+++ 2025-2026 2parte/Ripasso/liste/esempio_liste.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Nodo
+{
+	int numero;
+	struct Nodo *next;
+};
+
+void stampalista(struct Nodo* head)
+{
+	struct Nodo *nodo=head;
+	/*lista vuota?*/
+	if (nodo==NULL)
+	{
+		printf("La lista non ha elementi\n");
+	}
+	else
+	{
+		printf("Ecco la lista: \n");
+		/*stampa ciascun elemento*/
+		while(nodo==NULL)
+			{
+				printf("%d\n",nodo->numero );//cose da fare nel nodo che stai guardando a questa iterazione
+				nodo = nodo ->next;//contatore come negli arrey
+			}
+			printf("\n\n");
+	}
+
+}
+//funzione principale
+int main()
+{
+	struct *head=NULL;//testa lista
+	stampalista(head);
+	return 0;
+}
