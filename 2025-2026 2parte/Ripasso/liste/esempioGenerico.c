33ab65a83d228a6e811df44f104fbfa07bbcc176
theblezefire <marco.roncacci@gmail.com>
2025-10-02 15:28:33 +0200

    update liste generico esercizio

--- 2025-2026 2parte/Ripasso/liste/esempioGenerico.c
+++ 2025-2026 2parte/Ripasso/liste/esempioGenerico.c
@@ -14,15 +14,33 @@ struct nodo *diventoTesta(struct nodo *head)
     Pinizio->next = head;
     return Pinizio;
 }
+
 //inserisci coda
 struct nodo *diventaCoda(struct nodo *head)
 {
-    //nuovo blocco
-    struct nodo *PNuovoNodo=malloc(sizeof(struct nodo));
+    //nuovo blocco d-o-o-e
+    struct nodo *Pcoda=malloc(sizeof(struct nodo));
     printf("iniserisci elemento intero\n");
-    scanf("%d",&(PNuovoNodo->n));
+    scanf("%d",&(Pcoda->n));
     //il blocco diventa coda
-    PNuovoNodo->next=NULL;
+    Pcoda->next=NULL;
+
+    /*controlla lista*/
+    if (head==NULL)
+    {
+        head=Pcoda;
+    }
+    else
+    {
+        struct nodo *corrente=head;
+
+        while(corrente->next!=NULL)
+        {
+            corrente=corrente->next;
+        }
+        corrente->next=Pcoda;
+    }
+    return head;
 }
 void stampalista(struct nodo *head)
 {
@@ -43,11 +61,58 @@ void stampalista(struct nodo *head)
     
     
 }
-
+struct nodo *cancellaTesta(struct nodo* head)
+{
+    struct nodo *Successivo;
+    if (head==NULL)
+    {
+        printf("lista vuota non posso cancellare\n");
+        Successivo=NULL;
+    }
+    else
+    {
+        /*salva indirizzo del successivo*/
+        Successivo=head->next;
+        free(head);
+    }
+    return Successivo;
+    
+}
 int main()
 {
+    
+    int n;
+    printf("creazione testa vuota lista\n");
     struct nodo *head=NULL;
-    stampalista(head);
-    head= diventoTesta(head);
+    printf("creazione completata\n");
+    /*menu funzioni*/
+    while(n!=0){
+    printf("inserisci 1 per leggere la lista\n");
+    printf("inserisci 2 elemento in testa alla lista\n");
+    printf("inserisci 3 canccela elemento in testa alla lista\n");
+    printf("inserisci 4 inserisci coda alla lista\n");
+    printf("inserisci 0 per finire\n");
+    scanf("%d",&n);
+        if (n==1)
+        {
+            stampalista(head);
+        }
+    
+        if (n==2)
+        {
+            head= diventoTesta(head);
+        }
+
+        if (n==3)
+        {
+            head= cancellaTesta(head);
+        }
+
+        if (n==4)
+        {
+            head= diventaCoda(head);
+        }
+    }
+    
     return 0;
 }
