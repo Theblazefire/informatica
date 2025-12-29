6d30aef1f18ea0bc4a1e40dde13b22fd267f93be
Theblazefire <100469738+Theblazefire@users.noreply.github.com>
2025-12-29 09:39:45 +0100

    eso2

--- 2025-2026 2parte/Ripasso/stringhe/eso/StringheEsonero2017B/StringheEsonero2017B.c
+++ 2025-2026 2parte/Ripasso/stringhe/eso/StringheEsonero2017B/StringheEsonero2017B.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+int maiuscola(char c){
+	return (c>='A'&&c<='Z');
+}
+
+void remove_char(char* str, int pos){
+	int j=pos;
+	while(str[j]!='\0'){
+		str[j]=str[j+1];
+		j++;
+	}
+}
+void verifica(char* str){
+	int i=0;
+	while(str[i]!='\0'){
+		if(maiuscola(str[i])&&(str[i]==str[i+1]	)){
+			remove_char(str,i);
+		}else{
+			i++;
+		}
+	}
+}
 int main(){
 	char stringa[50];
 	printf("elimino le doppie maiuscile\n");
 	printf("inserisci una stringa\n");
 	fgets(stringa,50,stdin);
 	stringa[strlen(stringa)-1]='\0';
-	verifica()
+	verifica(stringa);
 	//appena riprendi lo studio sviluppa la funzione come nel primo esonero
-	// è simile 
+	// è simile
+	printf("%s", stringa); 
 }
